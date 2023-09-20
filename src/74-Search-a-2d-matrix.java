4ec23d76a8fd4078b6e42184d439a78487b44e02
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2023-09-20 19:34:36 +0600

    Added 74-Search-a-2d-matrix

---
+++ src/74-Search-a-2d-matrix.java
@@ -0,0 +1,38 @@
+//Solution 1
+class Solution1 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int row = 0;
+        int col = matrix[0].length-1;
+        while (row<matrix.length && col>=0){
+            if (matrix[row][col]==target)
+                return true;
+            else if (matrix[row][col]>target)
+                col--;
+            else
+                row++;
+        }
+        return false;
+    }
+}
+
+//Solution 2
+class Solution2 {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix.length==0)
+            return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int low = 0;
+        int high = row*col;
+        while (low<high){
+            int mid = (high+low)/2;
+            if (matrix[mid/col][mid%col]==target)
+                return true;
+            else if (matrix[mid/col][mid%col]>target)
+                high = mid;
+            else
+                low = mid+1;
+        }
+        return false;
+    }
+}
