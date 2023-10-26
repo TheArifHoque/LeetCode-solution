579947ffdb88afa9ba126726164a73307295645d
TheArifHoque <97823073+TheArifHoque@users.noreply.github.com>
2023-10-26 19:43:00 +0600

    Added 215-kth-largest-element-in-a-array

---
+++ src/215-kth-largest-element-in-a-array.java
@@ -0,0 +1,13 @@
+import java.util.PriorityQueue;
+
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        for (int i: nums) {
+            heap.add(i);
+            if (heap.size()>k)
+                heap.poll();
+        }
+        return heap.peek();
+    }
+}
