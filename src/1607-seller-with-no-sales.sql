ead577e8a368887ee5adf71a81ab1c22df122a48
Ariful Hoque <97823073+TheArifHoque@users.noreply.github.com>
2026-04-18 15:35:02 +0600

    Add SQL query to find sellers with no sales in 2020

---
+++ src/1607-seller-with-no-sales.sql
@@ -0,0 +1,8 @@
+SELECT s.seller_name
+FROM seller s
+WHERE s.seller_id NOT IN (
+    SELECT o.seller_id
+    FROM orders o
+    WHERE o.sale_date >= '2020-01-01' AND o.sale_date <= '2020-12-31'
+)
+ORDER BY s.seller_name ASC
